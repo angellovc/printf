b30329e3e6fdda826d42deddc82e801111457e79
Diego Gómez <dagomez2530@gmail.com>
2020-03-17 04:15:14 +0000

    1-print_double.c, print %d specifier function, 1-print_integer.c %i specifier function and _printf.c (main function)

---
+++ _printf.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _printf - prints int, char, string, float
+ * @format: string to print
+ * Return: int value with string length
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, len = 0;
+
+	va_list(list);
+	va_start(list, format);
+	for (i = 0; format != '\0' && format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			len = len + find(&format[i + 1], list);
+			i = i + 1;
+		}
+		else
+		{
+			print_char(format[i]);
+			len++;
+		}
+	}
+	va_end(list);
+	return (len);
+}
